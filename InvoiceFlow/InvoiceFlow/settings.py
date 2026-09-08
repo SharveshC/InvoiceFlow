7e2c4744d0913081ce745fba1598604c0f28374d
SharveshC <sharvesh1817@gmail.com>
2026-09-09 01:31:18 +0530

    feat(ui): implemented sign-in and sign-up backend

--- InvoiceFlow/InvoiceFlow/settings.py
+++ InvoiceFlow/InvoiceFlow/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hf344trx32c*13eg83^qtss6rttm9w%ik!0bacjb!&j))bogda'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +80,14 @@ WSGI_APPLICATION = 'InvoiceFlow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -122,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "/login/"
--- InvoiceFlow/myapp/views.py
+++ InvoiceFlow/myapp/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def home(request):
+    return render (request,'index.html')
+
+def login_page(request):
+    if request.method == "POST":
+
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email__iexact=email)
+            username = user.username
+        except User.DoesNotExist:
+            username = None
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+
+        return render(request, "login.html", {
+            "error": "Invalid email or password."
+        })
+        
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method == "POST":
+
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password != confirm_password:
+            return render(request, "signup.html", {
+                "error": "Passwords do not match."
+            })
+
+        if User.objects.filter(email__iexact=email).exists():
+            return render(request, "signup.html", {
+                "error": "An account with this email already exists."
+            })
+
+        username = email
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        user.first_name = full_name
+        user.save()
+
+        return redirect("login")
+    
+    return render(request,'signup.html')
